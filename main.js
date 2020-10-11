66547618ba390b184f9e68a75265ee6626610415
andybyers21 <andy.byers21@gmail.com>
2020-10-11 12:16:46 +0100

    clean code, add title & text

--- main.js
+++ main.js
@@ -2,17 +2,24 @@ const body = document.body;
 const textBox = document.querySelector('.text-box');
 const h1 = document.getElementsByTagName('h1')[0];
 const h2 = document.getElementsByTagName('h2')[0];
+const h3 = document.getElementsByTagName('h3')[0];
 const a = document.getElementsByTagName('a')[0];
+const p = document.getElementsByTagName('p')[0];
 
 function switchTheme() {
-	textBox.classList.toggle('dark-mode');
-	body.classList.toggle('dark-mode');
-	h1.classList.toggle('dark-mode');
-    h2.classList.toggle('dark-mode');
-    a.classList.toggle('dark-mode');
+  textBox.classList.toggle('dark-mode');
+  body.classList.toggle('dark-mode');
+  h1.classList.toggle('dark-mode');
+  h2.classList.toggle('dark-mode');
+  h3.classList.toggle('dark-mode');
+  a.classList.toggle('dark-mode');
+  p.classList.toggle('dark-mode');
 }
 
 // system ui
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-	switchTheme('dark-mode')
+if (
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+) {
+  switchTheme('dark-mode');
 }
